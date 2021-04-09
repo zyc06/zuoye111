f48fce7d44188cc193c3e787bccd66f7cf3b70d7
linzhi597 <1445148220@qq.com>
2021-04-09 13:40:59 +0800

    物联网作业

---
+++ app/src/main/java/com/huatec/hiot_cloud/MainActivity.java
@@ -0,0 +1,23 @@
+package com.huatec.hiot_cloud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.huatec.hiot_cloud.test.mvptest.model.User;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("tag","test");
+
+    }
+}
